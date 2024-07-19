6b64428f123d3686aef68b57ca7825ebec3e079f
karan17102003 <karanmehra17102003@gmail.com>
2024-07-19 21:42:20 +0530

    3commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express=require("express");
 const mongoose=require("mongoose");
 const jobRoutes=require("./route/job")
 const app=express();
-mongoose.connect("mongodb://localhost:27017/job_app")
-.then(()=> console.log("db connected"))
-.catch(()=> console.log("error",err))
+mongoose.connect("mongodb+srv://karanmehra17102003:USewdzRT8epmGavL@cluster0.b8fysc8.mongodb.net/")
+
+
 app.use(express.json());
 app.use(jobRoutes);
 app.listen(10_000,()=>console.log('server is up and running at port 10000'));
